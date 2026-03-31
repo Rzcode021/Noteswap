e88ab9928fd3bd05d13184b8f795a25b02c4342e
Raunak khan <khanraunak0021@gmail.com>
2026-04-01 00:40:53 +0530

    some changes

--- frontend/middleware.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export default function middleware(request) {
-  // Middleware disabled - using proxy configuration instead
-  // Next.js 16 deprecated middleware.js files
-  return NextResponse.next()
-}
