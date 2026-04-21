496740711cbae6541f0e99a3b1ca0b615a8283dd
Raunak khan <khanraunak0021@gmail.com>
2026-04-21 13:09:23 +0530

    cors change

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,7 @@ app.use(cors({
     const allowedOrigins = [
       'http://localhost:3000',
       'http://localhost:5173',
-      'https://notesswap-mu.vercel.app',
+      "https://note-swap.smarttechbros.com/",
       process.env.CLIENT_URL,
     ].filter(Boolean)
     if (allowedOrigins.includes(origin) || origin.endsWith('.vercel.app')) {
